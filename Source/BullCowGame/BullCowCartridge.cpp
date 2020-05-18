9dbd1734fc969d9ed9434709d097126205132d18
guipbo <guipbo@gmail.com>
2020-05-18 15:01:45 -0300

    BasicLoop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,26 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the game."));
+    PrintLine(TEXT("Guess the 4 letter word!"));
+
+    InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    
+    ClearScreen();
+
+    if(HiddenWord!=Input){
+        PrintLine(TEXT("You have lost!"));
+        Lives -= 1;
+        return;
+    }
+    PrintLine(TEXT("You have won!"));
+}
+
+void UBullCowCartridge::InitGame(){
+
+    HiddenWord = TEXT("cake"); //Set the HiddenWord
+    Lives = 3; //Set number of lives
 }
